47450454fc6448646f6dafc0fcc312a52da5a2a0
amiraallagui <allagui_fawzi@outlook.fr>
2023-12-01 16:11:41 +0100

    Accessing query Parameters from the request

--- src/home/home.controller.ts
+++ src/home/home.controller.ts
@@ -1,13 +1,19 @@
-import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Query } from '@nestjs/common';
 import { HomeService } from './home.service';
 import { HomeResponseDto } from './dto/home.dto';
+import { propertyType } from '@prisma/client';
 
 @Controller('home')
 export class HomeController {
   constructor(private readonly homeService: HomeService) {}
 
   @Get()
-  getHomes(): Promise<HomeResponseDto[]> {
+  getHomes(
+    @Query('city') city?: string,
+    @Query('minPrice') minPrice?: string,
+    @Query('maxPrice') maxPrice?: string,
+    @Query('propertyType') propertyType?: propertyType,
+  ): Promise<HomeResponseDto[]> {
     return this.homeService.getHomes();
   }
 
