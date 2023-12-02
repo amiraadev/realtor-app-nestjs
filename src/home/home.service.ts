0374cad12dea916c1ac806a841a4940e63d012dd
amiraallagui <allagui_fawzi@outlook.fr>
2023-12-02 21:07:47 +0100

    Send back more useful Info

--- src/home/home.service.ts
+++ src/home/home.service.ts
@@ -183,6 +183,16 @@ export class HomeService {
       where: {
         home_id: homeId,
       },
+      select: {
+        message: true,
+        buyer: {
+          select: {
+            name: true,
+            phone: true,
+            email: true,
+          },
+        },
+      },
     });
   }
 }
