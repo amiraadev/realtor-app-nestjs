c23c0687e0d3163e0e3965e4a59853e96baed61c
amiraallagui <allagui_fawzi@outlook.fr>
2023-12-02 21:34:34 +0100

    the structure of the test file

--- src/home/home.service.spec.ts
+++ src/home/home.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HomeService } from './home.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 describe('HomeService', () => {
   let service: HomeService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [HomeService],
+      providers: [HomeService, PrismaService],
     }).compile();
 
     service = module.get<HomeService>(HomeService);
