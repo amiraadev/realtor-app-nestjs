95144caf5ad2a5267b06705b51b476e258130189
amiraallagui <allagui_fawzi@outlook.fr>
2023-12-02 19:42:11 +0100

    Accessing the User from the DB

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -3,10 +3,20 @@ import { CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Injectable } from '@nestjs/common';
 import * as jwt from "jsonwebtoken"
+import { PrismaService } from 'src/prisma/prisma.service';
 
+interface JWTPayload {
+    name:string;
+    id:number;
+    iat:number;
+    exp:number;
+}
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private readonly reflector: Reflector) {}
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly prismaService: PrismaService
+    ) {}
   async canActivate(context: ExecutionContext) {
     const roles = this.reflector.getAllAndOverride('roles', [
       context.getHandler(),
@@ -16,9 +26,17 @@ export class AuthGuard implements CanActivate {
         const request = context.switchToHttp().getRequest();
         const token = request?.headers?.authorization?.split('Bearer ')[1];
         try {
-           const user = await jwt.verify(token,process.env.TOKEN_SECRET)
-           console.log(user); 
-           return true;   
+           const payload = await jwt.verify(token,process.env.TOKEN_SECRET) as JWTPayload
+           const user = await this.prismaService.user.findUnique({
+            where:{
+                id:payload.id
+            }
+           })
+           if(!user){
+            return false;   
+           }
+           if(roles.includes(user.user_type)) return true
+           return false;   
         } catch (error) {
             return false;   
         }
